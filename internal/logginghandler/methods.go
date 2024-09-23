e669719c18b5efdc92c8909af163ac6a22c5c72a
Artemij Belyakov <bart82@mail.ru>
2024-09-23 17:36:28 +0300

    23.09.2024 	modified:   cmd/main.go 	modified:   cmd/webhookserver/methods.go 	modified:   cmd/webhookserver/routes.go 	modified:   cmd/webhookserver/types.go 	modified:   cmd/webhookserver/webhookserver_test.go 	modified:   internal/logginghandler/type.go 	modified:   test/testwebhookserver/testwebhookserver_test.go

---
+++ internal/logginghandler/methods.go
@@ -0,0 +1,18 @@
+package logginghandler
+
+func New() *LoggingChan {
+	return &LoggingChan{
+		logChan: make(chan MessageLogging),
+	}
+}
+
+func (l *LoggingChan) GetChan() <-chan MessageLogging {
+	return l.logChan
+}
+
+func (l *LoggingChan) Send(msgType, msgData string) {
+	l.logChan <- MessageLogging{
+		MsgType: msgType,
+		MsgData: msgData,
+	}
+}
