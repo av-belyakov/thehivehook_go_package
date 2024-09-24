b5cc5a4f03500048c16baee62733d84828d0431b
Artemij Belyakov <bart82@mail.ru>
2024-09-24 13:13:07 +0300

    24.09.2024 	modified:   cmd/webhookserver/webhookserver_test.go

--- cmd/webhookserver/webhookserver_test.go
+++ cmd/webhookserver/webhookserver_test.go
@@ -2,7 +2,6 @@ package webhookserver_test
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,6 +10,8 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"reflect"
+	"strings"
 	"syscall"
 	"testing"
 )
@@ -97,9 +98,18 @@ func (wh *WebHookServer) RouteWebHook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println("__________", string(bodyByte), "___________")
+	//fmt.Println("__________", string(bodyByte), "___________")
 
-	dst := make([]byte, base64.StdEncoding.DecodedLen(len(bodyByte)))
+	str, err := NewReadReflectJSONSprint(bodyByte)
+	if err != nil {
+		fmt.Println("ERROR:", err.Error())
+
+		return
+	}
+
+	fmt.Println("__________", str, "___________")
+
+	/*dst := make([]byte, base64.StdEncoding.DecodedLen(len(bodyByte)))
 	strData, err := base64.StdEncoding.Decode(dst, bodyByte)
 	if err != nil {
 		fmt.Println("ERROR:", err.Error())
@@ -114,7 +124,7 @@ func (wh *WebHookServer) RouteWebHook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(string(data))
+	fmt.Println(string(data))*/
 }
 
 func TestWebhookServer(t *testing.T) {
@@ -138,4 +148,156 @@ func TestWebhookServer(t *testing.T) {
 	}
 
 	webHookServer.Start()
+}
+
+// NewReadReflectJSONSprint функция выполняет вывод JSON сообщения в виде текста
+// Для данной функции не требуется описание текст, так как обработка JSON сообщения
+// осуществляется с помощью пакета reflect
+func NewReadReflectJSONSprint(b []byte) (string, error) {
+	var str string
+	errSrc := "error decoding the json file, it may be empty"
+
+	listMap := map[string]interface{}{}
+	if err := json.Unmarshal(b, &listMap); err == nil {
+		if len(listMap) == 0 {
+			return str, fmt.Errorf(errSrc)
+		}
+
+		return readReflectMapSprint(listMap, 0), err
+	}
+
+	listSlice := []interface{}{}
+	if err := json.Unmarshal(b, &listSlice); err == nil {
+		if len(listSlice) == 0 {
+			return str, fmt.Errorf(errSrc)
+		}
+
+		return readReflectSliceSprint(listSlice, 0), err
+	}
+
+	return str, fmt.Errorf("the contents of the file are not Map or Slice")
+}
+
+func readReflectAnyTypeSprint(name interface{}, anyType interface{}, num int) string {
+	var (
+		nameStr string
+		str     strings.Builder = strings.Builder{}
+	)
+
+	r := reflect.TypeOf(anyType)
+	ws := GetWhitespace(num)
+
+	if n, ok := name.(int); ok {
+		nameStr = fmt.Sprintf("%s%v.", ws, n+1)
+	} else if n, ok := name.(string); ok {
+		nameStr = fmt.Sprintf("%s\"%s\":", ws, n)
+	}
+
+	if r == nil {
+		return str.String()
+	}
+
+	switch r.Kind() {
+	case reflect.String:
+		str.WriteString(fmt.Sprintf("%s \"%s\"\n", nameStr, reflect.ValueOf(anyType).String()))
+
+	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64:
+		str.WriteString(fmt.Sprintf("%s %d\n", nameStr, reflect.ValueOf(anyType).Int()))
+
+	case reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		str.WriteString(fmt.Sprintf("%s %d\n", nameStr, reflect.ValueOf(anyType).Uint()))
+
+	case reflect.Float32, reflect.Float64:
+		str.WriteString(fmt.Sprintf("%s %v\n", nameStr, int(reflect.ValueOf(anyType).Float())))
+
+	case reflect.Bool:
+		str.WriteString(fmt.Sprintf("%s %v\n", nameStr, reflect.ValueOf(anyType).Bool()))
+	}
+
+	return str.String()
+}
+
+func readReflectMapSprint(list map[string]interface{}, num int) string {
+	var str strings.Builder = strings.Builder{}
+	ws := GetWhitespace(num)
+
+	for k, v := range list {
+		r := reflect.TypeOf(v)
+
+		if r == nil {
+			return str.String()
+		}
+
+		switch r.Kind() {
+		case reflect.Map:
+			if v, ok := v.(map[string]interface{}); ok {
+				str.WriteString(fmt.Sprintf("%s%s:\n", ws, k))
+				str.WriteString(readReflectMapSprint(v, num+1))
+			}
+
+		case reflect.Slice:
+			if v, ok := v.([]interface{}); ok {
+				str.WriteString(fmt.Sprintf("%s%s:\n", ws, k))
+				str.WriteString(readReflectSliceSprint(v, num+1))
+			}
+
+		case reflect.Array:
+			str.WriteString(fmt.Sprintf("%s: %s (it is array)\n", k, reflect.ValueOf(v).String()))
+
+		default:
+			str.WriteString(readReflectAnyTypeSprint(k, v, num))
+		}
+	}
+
+	return str.String()
+}
+
+func readReflectSliceSprint(list []interface{}, num int) string {
+	var str strings.Builder = strings.Builder{}
+	ws := GetWhitespace(num)
+
+	for k, v := range list {
+		r := reflect.TypeOf(v)
+
+		if r == nil {
+			return str.String()
+		}
+
+		switch r.Kind() {
+		case reflect.Map:
+			if v, ok := v.(map[string]interface{}); ok {
+				str.WriteString(fmt.Sprintf("%s%d.\n", ws, k+1))
+				str.WriteString(readReflectMapSprint(v, num+1))
+			}
+
+		case reflect.Slice:
+			if v, ok := v.([]interface{}); ok {
+				str.WriteString(fmt.Sprintf("%s%d.\n", ws, k+1))
+				str.WriteString(readReflectSliceSprint(v, num+1))
+			}
+
+		case reflect.Array:
+			str.WriteString(fmt.Sprintf("%d. %s (it is array)\n", k, reflect.ValueOf(v).String()))
+
+		default:
+			str.WriteString(readReflectAnyTypeSprint(k, v, num))
+		}
+	}
+
+	return str.String()
+}
+
+// GetWhitespace возвращает необходимое количество пробелов
+func GetWhitespace(num int) string {
+	var str string
+
+	if num == 0 {
+		return str
+	}
+
+	for i := 0; i < num; i++ {
+		str += "  "
+	}
+
+	return str
 }
