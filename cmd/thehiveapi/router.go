5f2ace0285e897fbfa9c847a6ab743f505f27a7c
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-02-13 17:54:49 +0300

    13.02.2025 	modified:   cmd/thehiveapi/router.go

--- cmd/thehiveapi/router.go
+++ cmd/thehiveapi/router.go
@@ -9,6 +9,243 @@ import (
 	"github.com/av-belyakov/thehivehook_go_package/internal/supportingfunctions"
 )
 
+/*
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go Observable STOP
+func 'CreateEvenCase', STOP...
+___ func 'RouteWebHook', AFTER func 'CreateEvenCase'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go Observable STOP
+func 'CreateEvenCase', STOP...
+___ func 'RouteWebHook', AFTER func 'CreateEvenCase'
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go Observable STOP
+func 'CreateEvenCase', STOP...
+___ func 'RouteWebHook', AFTER func 'CreateEvenCase'
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+func 'CreateEvenCase', STOP...
+2025-02-13 17:43:56 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go Observable STOP
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+func 'CreateEvenCase', STOP...
+___ func 'RouteWebHook', AFTER func 'CreateEvenCase'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go Observable STOP
+func 'CreateEvenCase', STOP...
+___ func 'RouteWebHook', AFTER func 'CreateEvenCase'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go Observable STOP
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+func 'CreateEvenCase', STOP...
+___ func 'RouteWebHook', AFTER func 'CreateEvenCase'
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~88356888792' START
+func 'CreateEvenCase', STOP...
+2025-02-13 17:44:25 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:44:26 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go TTP STOP
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEventCase', goroutine 'observable' received data
+__________________ goroutine g.Go Observable STOP
+func 'CreateEvenCase', STOP...
+___ func 'RouteWebHook', AFTER func 'CreateEvenCase'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:44:40 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_ttp', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893435000' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893435000' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:45:23 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:45:26 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893435000' START
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893435000' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893435000' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:45:42 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+func 'CreateEvenCase', STOP...
+2025-02-13 17:45:43 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:45:44 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893029496' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893029496' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:46:26 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:46:27 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893029496' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90893029496' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:46:54 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:47:01 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90320322744' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90320322744' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90320322744' START
+func 'CreateEvenCase', STOP...
+2025-02-13 17:47:22 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90320322744' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+___ func 'RouteWebHook', BEFORE func 'CreateEvenCase'
+!!! func 'CreateEvenCase', root id:'~90320322744' START
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:47:33 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:47:35 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:47:42 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+func 'CreateEvenCase', STOP...
+2025-02-13 17:47:44 ERR - thehivehook_go_package - context deadline exceeded /home/artemij/go/src/thehivehook_go_package/cmd/webhookserver/routes.go:53
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+=== func 'router', command:'get_observables', root id:'~88356888792'
+*/
+
 func (api *apiTheHiveModule) router(ctx context.Context) {
 	for {
 		select {
