f2dee62a2f1fe0cdf31382635aa0c63037b88c09
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-05-05 17:21:30 +0300

    Сделать своё отдельное кеширующее хранилище
    Сделал отдельное кеширующее хранилище, на пробу и отключил
    хранилище из пакета cachingstoragewithqueue
    	modified:   cmd/thehiveapi/app.go
    	modified:   cmd/thehiveapi/router.go
    	deleted:    cmd/thehiveapi/storage.go
    	new file:   cmd/thehiveapi/storage/app.go
    	new file:   cmd/thehiveapi/storage/app_test.go
    	new file:   cmd/thehiveapi/storage/methods.go
    	new file:   cmd/thehiveapi/storage/types.go
    	modified:   cmd/thehiveapi/types.go
    	modified:   version

---
+++ cmd/thehiveapi/storage/app.go
@@ -0,0 +1,94 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+// NewStorageFoundObjects инициализация кеширующего хранилища объектов
+func NewStorageFoundObjects(opts ...cacheOptions) (*StorageFoundObjects, error) {
+	storage := &StorageFoundObjects{
+		//значение по умолчанию для интервала автоматической обработки
+		timeTick: time.Duration(5 * time.Second),
+		//значение по умолчанию для времени жизни объекта
+		maxTtl: time.Duration(3600 * time.Second),
+		//хранилище объектов
+		storages: storage{
+			foundObjects: map[string]foundObject{},
+		},
+	}
+
+	for _, opt := range opts {
+		if err := opt(storage); err != nil {
+			return storage, err
+		}
+	}
+
+	return storage, nil
+}
+
+func (s *StorageFoundObjects) Start(ctx context.Context) {
+	go func() {
+		tick := time.NewTicker(s.timeTick)
+		defer tick.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("Breack!!!")
+
+				return
+
+			case <-tick.C:
+				//поиск и удаление из хранилища всех объектов у которых истекло время жизни
+				s.DeleteForTimeExpiryObjectFromCache()
+			}
+		}
+	}()
+}
+
+//***************** методы для настройки хранилища ******************
+
+// WithMaxTtl устанавливает максимальное время, по истечении которого запись в cacheStorages будет
+// удалена, допустимый интервал времени хранения записи от 5 до 3600 секунд
+func WithMaxTtl(v int) cacheOptions {
+	return func(s *StorageFoundObjects) error {
+		if v < 5 || v > 3600 {
+			return errors.New("the maximum time after which an entry in the cache will be deleted should not be less than 300 seconds or more than 24 hours (86400 seconds)")
+		}
+
+		s.maxTtl = time.Duration(v) * time.Second
+
+		return nil
+	}
+}
+
+// WithTimeTick устанавливает интервал времени, заданное время такта, по истечении которого
+// запускается новый виток автоматической обработки содержимого кэша, интервал значений должен
+// быть в диапазоне от 1 до 120 секунд
+func WithTimeTick(v int) cacheOptions {
+	return func(s *StorageFoundObjects) error {
+		if v < 1 || v > 120 {
+			return errors.New("the set clock cycle time should not be less than 3 seconds or more than 120 seconds")
+		}
+
+		s.timeTick = time.Duration(v) * time.Second
+
+		return nil
+	}
+}
+
+// WithMaxSize устанавливает максимальный размер кэша, не может быть меньше 3 и больше 1000
+func WithMaxSize(v int) cacheOptions {
+	return func(s *StorageFoundObjects) error {
+		if v < 3 || v > 1000 {
+			return errors.New("the maximum cache size cannot be less than 3 or more than 1000 objects")
+		}
+
+		s.maxSize = v
+
+		return nil
+	}
+}
