fc23c5e9d60a11a7018f944fd5d2beca4f52e02b
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2026-01-23 10:50:18 +0300

    fix: Устранить подвисание кейса при больщой нагрузке 	modified:   cmd/thehiveapi/app.go 	modified:   cmd/thehiveapi/router.go 	modified:   version
    	modified:   cmd/thehiveapi/app.go
    	modified:   cmd/thehiveapi/router.go
    	modified:   cmd/webhookserver/routes.go
    	modified:   version

--- cmd/thehiveapi/router.go
+++ cmd/thehiveapi/router.go
@@ -16,118 +16,6 @@ func (api *apiTheHiveModule) router(ctx context.Context) {
 
 		case msg := <-api.receivingChannel:
 			switch msg.GetCommand() {
-			case "get_observables":
-				keyId := msg.GetCommand() + msg.GetRootId()
-
-				so := NewSpecialObjectForCache[any]()
-				so.SetID(keyId)
-
-				//для того что бы выполнить сравнение объектов нужно передать
-				//этот объект so.SetObject
-				//хотя для thehivehook_go может это не надо, надо обдумать!!!
-				//so.SetObject(msg.GetData())
-
-				so.SetFunc(func(_ int) bool {
-					api.logger.Send("info", fmt.Sprintf("start search object, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
-
-					newRes := NewChannelRespons()
-					newRes.SetRequestId(msg.GetRequestId())
-					newRes.SetStatusCode(200)
-
-					//ищем в хранилище объект, который возможно уже запрашивали ранее
-					if obj, ok := api.storageCache.GetObject(keyId); ok {
-						api.logger.Send("info", fmt.Sprintf("the object with id:'%s' was found in the cache", keyId))
-
-						newRes.SetData(obj)
-					} else {
-						api.logger.Send("info", fmt.Sprintf("request to TheHive, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
-
-						//делаем запрос к TheHive для получения дополнительной информации по объекту
-						res, statusCode, err := api.GetObservables(ctx, msg.GetRootId())
-						if err != nil {
-							api.logger.Send("error", supportingfunctions.CustomError(err).Error())
-						} else {
-							api.logger.Send("info", fmt.Sprintf("successful response to TheHive request, command:'%s', root id:'%s', status code:'%d'", msg.GetCommand(), msg.GetRootId(), statusCode))
-
-							// добавляем найденный объект в кеш
-							api.storageCache.SetObject(keyId, res)
-							newRes.SetData(res)
-						}
-
-						newRes.SetStatusCode(statusCode)
-					}
-
-					select {
-					case <-msg.GetContext().Done():
-						return false
-
-					default:
-						msg.GetChanOutput() <- newRes
-
-					}
-
-					return true
-				})
-
-				//добавляем объект в очередь для обработки
-				api.cache.PushObjectToQueue(so)
-
-			case "get_ttp":
-				keyId := msg.GetCommand() + msg.GetRootId()
-
-				so := NewSpecialObjectForCache[any]()
-				so.SetID(keyId)
-
-				//для того что бы выполнить сравнение объектов нужно передать
-				//этот объект so.SetObject
-				//хотя для thehivehook_go может это не надо, надо обдумать!!!
-				//so.SetObject(msg.GetData())
-
-				so.SetFunc(func(_ int) bool {
-					api.logger.Send("info", fmt.Sprintf("start search object, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
-
-					newRes := NewChannelRespons()
-					newRes.SetRequestId(msg.GetRequestId())
-					newRes.SetStatusCode(200)
-
-					//ищем в хранилище объект, который возможно уже запрашивали ранее
-					if obj, ok := api.storageCache.GetObject(keyId); ok {
-						api.logger.Send("info", fmt.Sprintf("the object with id:'%s' was found in the cache", keyId))
-
-						newRes.SetData(obj)
-					} else {
-						api.logger.Send("info", fmt.Sprintf("request to TheHive, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
-
-						//делаем запрос к TheHive для получения дополнительной информации по объекту
-						res, statusCode, err := api.GetTTP(ctx, msg.GetRootId())
-						if err != nil {
-							api.logger.Send("error", supportingfunctions.CustomError(err).Error())
-						} else {
-							api.logger.Send("info", fmt.Sprintf("successful response to TheHive request, command:'%s', root id:'%s', status code:'%d'", msg.GetCommand(), msg.GetRootId(), statusCode))
-
-							// добавляем найденный объект в кеш
-							api.storageCache.SetObject(keyId, res)
-							newRes.SetData(res)
-						}
-
-						newRes.SetStatusCode(statusCode)
-					}
-
-					select {
-					case <-msg.GetContext().Done():
-						return false
-
-					default:
-						msg.GetChanOutput() <- newRes
-
-					}
-
-					return true
-				})
-
-				//добавляем объект в очередь для обработки
-				api.cache.PushObjectToQueue(so)
-
 			case "get_alert":
 				keyId := msg.GetCommand() + msg.GetRootId()
 
@@ -160,6 +48,122 @@ func (api *apiTheHiveModule) router(ctx context.Context) {
 							api.storageCache.SetObject(keyId, res)
 							newRes.SetData(res)
 						}
+
+						newRes.SetStatusCode(statusCode)
+					}
+
+					select {
+					case <-msg.GetContext().Done():
+						return false
+
+					default:
+						msg.GetChanOutput() <- newRes
+
+					}
+
+					return true
+				})
+
+				//добавляем объект в очередь для обработки
+				api.cache.PushObjectToQueue(so)
+
+			case "get_observables":
+				keyId := msg.GetCommand() + msg.GetRootId()
+
+				so := NewSpecialObjectForCache[any]()
+				so.SetID(keyId)
+
+				api.logger.Send("info", fmt.Sprintf("apiTheHiveModule.router the command has been accepted, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
+
+				//для того что бы выполнить сравнение объектов нужно передать
+				//этот объект so.SetObject
+				//хотя для thehivehook_go может это не надо, надо обдумать!!!
+				//so.SetObject(msg.GetData())
+
+				so.SetFunc(func(_ int) bool {
+					api.logger.Send("info", fmt.Sprintf("start function execution, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
+
+					newRes := NewChannelRespons()
+					newRes.SetRequestId(msg.GetRequestId())
+					newRes.SetStatusCode(200)
+
+					//ищем в хранилище объект, который возможно уже запрашивали ранее
+					//if obj, ok := api.storageCache.GetObject(keyId); ok {
+					//	api.logger.Send("info", fmt.Sprintf("the object with id:'%s' was found in the cache", keyId))
+					//
+					//	newRes.SetData(obj)
+					//} else {
+					api.logger.Send("info", fmt.Sprintf("request to TheHive, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
+
+					//делаем запрос к TheHive для получения дополнительной информации по объекту
+					res, statusCode, err := api.GetObservables(ctx, msg.GetRootId())
+					if err != nil {
+						api.logger.Send("error", supportingfunctions.CustomError(err).Error())
+					} else {
+						api.logger.Send("info", fmt.Sprintf("successful response to TheHive request, command:'%s', root id:'%s', status code:'%d'", msg.GetCommand(), msg.GetRootId(), statusCode))
+
+						// добавляем найденный объект в кеш
+						//		api.storageCache.SetObject(keyId, res)
+						newRes.SetData(res)
+					}
+
+					newRes.SetStatusCode(statusCode)
+					//}
+
+					select {
+					case <-msg.GetContext().Done():
+						return false
+
+					default:
+						msg.GetChanOutput() <- newRes
+
+					}
+
+					return true
+				})
+
+				//добавляем объект в очередь для обработки
+				api.cache.PushObjectToQueue(so)
+
+			case "get_ttp":
+				keyId := msg.GetCommand() + msg.GetRootId()
+
+				so := NewSpecialObjectForCache[any]()
+				so.SetID(keyId)
+
+				api.logger.Send("info", fmt.Sprintf("apiTheHiveModule.router the command has been accepted, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
+
+				//для того что бы выполнить сравнение объектов нужно передать
+				//этот объект so.SetObject
+				//хотя для thehivehook_go может это не надо, надо обдумать!!!
+				//so.SetObject(msg.GetData())
+
+				so.SetFunc(func(_ int) bool {
+					api.logger.Send("info", fmt.Sprintf("start function execution, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
+
+					newRes := NewChannelRespons()
+					newRes.SetRequestId(msg.GetRequestId())
+					newRes.SetStatusCode(200)
+
+					//ищем в хранилище объект, который возможно уже запрашивали ранее
+					//if obj, ok := api.storageCache.GetObject(keyId); ok {
+					//	api.logger.Send("info", fmt.Sprintf("the object with id:'%s' was found in the cache", keyId))
+					//
+					//	newRes.SetData(obj)
+					//} else {
+					api.logger.Send("info", fmt.Sprintf("request to TheHive, command:'%s', root id:'%s' (case:'%s')", msg.GetCommand(), msg.GetRootId(), msg.GetCaseId()))
+
+					//делаем запрос к TheHive для получения дополнительной информации по объекту
+					res, statusCode, err := api.GetTTP(ctx, msg.GetRootId())
+					if err != nil {
+						api.logger.Send("error", supportingfunctions.CustomError(err).Error())
+					} else {
+						api.logger.Send("info", fmt.Sprintf("successful response to TheHive request, command:'%s', root id:'%s', status code:'%d'", msg.GetCommand(), msg.GetRootId(), statusCode))
+
+						// добавляем найденный объект в кеш
+						//		api.storageCache.SetObject(keyId, res)
+						newRes.SetData(res)
+						//	}
 
 						newRes.SetStatusCode(statusCode)
 					}
